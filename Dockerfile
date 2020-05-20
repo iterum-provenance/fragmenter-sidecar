f8baac53f5185f794c94a0cda7252a10bcd48b7a
Mantsje <edserapperloo@hotmail.com>
2020-05-20 11:44:49 +0200

    update dependencies

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ RUN mkdir /app
 ADD . /app/
 WORKDIR /app
 RUN go build -o fragmenter-sidecar .
+
 CMD ["/app/fragmenter-sidecar"]
--- test.sh
+++ test.sh
@@ -1,26 +1,5 @@
 
-export ITERUM_NAME="fragmenter-sidecar"
-export DATA_VOLUME_PATH="/tmp/"
-export PIPELINE_HASH="asdh37dHsf8H3fSSd24HEe35g2d4h754"
-export MANAGER_URL="http://dummyurl.com"
-
-
-export DAEMON_URL="http://localhost:3000"
-export DAEMON_DATASET="test_dataset"
-export DAEMON_COMMIT_HASH="97QVlptzj81zTuzJRhm1jUhqDi9rr1dF"
-
-export MQ_BROKER_URL="amqp://iterum:sinaasappel@localhost:5672"
-export MQ_INPUT_QUEUE="INVALID"
-export MQ_OUTPUT_QUEUE="frag-out"
-
-export MINIO_URL="localhost:9000"
-export MINIO_ACCESS_KEY="iterum"
-export MINIO_SECRET_KEY="banaanappel"
-export MINIO_USE_SSL="false"
-export MINIO_OUTPUT_BUCKET="frag-out"
-
-export FRAGMENTER_INPUT="tf.sock"
-export FRAGMENTER_OUTPUT="ff.sock"
+source test.env
 
 make build
 fragmenter-sidecar
